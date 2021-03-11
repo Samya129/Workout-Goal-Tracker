c31e35a03d143cb4f9245a6d9a98e9dbca20176f
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-03-11 01:16:58 -0500

    Pseudo coded server.js file for clarity.

--- server.js
+++ server.js
@@ -8,18 +8,23 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//Activate morgan
 app.use(logger("dev"));
 
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+//Connect to mongoose with the database name I defined
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDb", { useNewUrlParser: true });
 
+//Require each route below for use:
 app.use(require("./routes/api.js"))
 app.use(require("./routes/view.js"))
 
+//Run server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
