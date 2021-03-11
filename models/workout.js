9c103040f73ac31e7a3663aae8e12bcee07d00ef
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-03-11 01:51:34 -0500

    Created the workout model schema based on the requirements from the readme and added mongoose dependency to this model file along with requiring the schema with mongoose.

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+//Require dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create a workout collection??
+const WorkoutSchema = new Schema({
+  //For the following exercises:  
+  exercises: [ 
+      {
+        name: {
+          type: String,
+          trim: true,
+        },
+        type: {
+          type: String,
+          trim: true,
+        },
+        duration: Number, //creating a constructor of number to call later for the duration total of the said workout.
+        weight: {
+          type: Number,
+          trim: true,
+          //Using a default for exercises that perhaps do not consist of using any weights, sets, reps, and distance.
+          default: 0
+        },
+        sets: {
+          type: Number,
+          trim: true,
+          default: 0
+        },
+        reps: {
+          type: Number,
+          trim: true,
+          default: 0
+        },
+        //For cardio exercise, want to track the distance traveled.
+        distance: {
+          type: Number,
+          trim: true,
+          default: 0
+        }
+      }
+    ],
+  });
+  
+  //need to call workoutSchema somehow to highlight and activate it...
+  
+
