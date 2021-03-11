6fb558bbbfb7c407cff4e12585a8a0774e1bfd52
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-03-11 00:59:10 -0500

    Added necessary dependencies and all the get requests for each of the routes according to the front end js files of code.

---
+++ routes/views.js
@@ -0,0 +1,21 @@
+//Required dependencies:
+var path = require("path");
+const router = require("express").Router();
+
+//If other or not specified, take to home route (default):
+router.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+//Automatic home route:
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+//Exercise route:
+router.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+})
+//Stats route:
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+})
+
